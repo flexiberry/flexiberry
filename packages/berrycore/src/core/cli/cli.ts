113616f43fa21847a039816550cd8b95ed76f26b
Rintu Raj.c <rinturajc180000@gmail.com>
2025-02-21 11:54:50 +0530

    ✨ Refactor berrycore package: remove unused dependencies

--- packages/berrycore/src/core/cli/cli.ts
+++ packages/berrycore/src/core/cli/cli.ts
@@ -1,122 +1,12 @@
-import readline from "readline";
-import { showLoadingSpinner } from "../util/loader";
-import { BerryCore } from "../berrycore";
-import { BerryRunner } from "../util/runner";
-import { RUNNER_EVENT } from "../../enum/runner.event";
-import { FileUtils } from "../functions/file";
-import { Lexer } from "../../lang/tokenizer/lexer";
-import { TokenTypeValueOf } from "../../lang/tokenizer/tokenType";
 import { printTable } from "console-table-printer";
+import { Lexer } from "../../lang/tokenizer/lexer";
+import { FileUtils } from "../functions/file";
+import { TokenTypeValueOf } from "../../lang/tokenizer/tokenType";
 import { CProgramBody } from "../../lang/ast/AstImpl";
 import { Producer } from "../producer/producer";
 import { Test } from "./test";
-import chalk from "chalk";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const optionsList = [
-  "Exit",
-  "Parse File and convert to Obj",
-  "Run test suite ",
-  "Parse Lexer",
-];
-
-function terminateProgram() {
-  console.log(chalk.red("Terminating program..."));
-  rl.close();
-  process.exit(0);
-}
-
-function displayOptions() {
-  console.log(chalk.blue("Please select an option:"));
-  optionsList.forEach((option, index) => {
-    console.log(chalk.green(`${index}`), chalk.white(`: ${option}`));
-  });
-}
-
-async function handleChoice(choice: number) {
-  if (choice === 0) {
-    terminateProgram();
-  } else if (choice > 0 && choice < optionsList.length) {
-    console.log(chalk.blackBright(`You selected: ${optionsList[choice]}`));
-
-    if (choice == 1) await parseFile();
-    if (choice == 2) await runRunner();
-    if (choice == 3) await runLexer();
-    askQuestion();
-  } else {
-    console.log(chalk.red("Invalid option, try again."));
-    askQuestion();
-  }
-}
-
-function askQuestion() {
-  displayOptions();
-  rl.question(
-    chalk.blue("Enter your choice (number): "),
-    async (input: string) => {
-      const choice = parseInt(input);
-      await handleChoice(choice);
-    }
-  );
-}
-
-export function handleInput(options: any) {
-  if (options.input) {
-    askQuestion();
-  }
-}
-
-async function runRunner() {
-  const eventManager = new BerryRunner();
-
-  const berryCore = new BerryCore();
-
-  // Subscribe to 'myEvent' event
-  eventManager
-    .on(RUNNER_EVENT.START, async (data: any) => {
-      console.log("Event START:", data);
-    })
-    .on(RUNNER_EVENT.ERROR, async (data: any) => {
-      console.log("Test ERROR:", data);
-    });
-  // .on(RUNNER_EVENT.COMPLETED, async (data: any) => {
-  //   console.log("Test COMPLETED:", data);
-  // });
-
-  await eventManager.run(null, "development");
-}
-
-async function parseFile(): Promise<any> {
-  return new Promise((resolve, reject) => {
-    rl.question(
-      chalk.blue("Please enter the input file path or type 'exit' to quit: "),
-      async (input: string) => {
-        if (input.toLowerCase() === "exit") {
-          terminateProgram();
-          resolve(null);
-        } else {
-          try {
-            showLoadingSpinner();
-            // const berryCore = new BerryCore();
-            // let result = await berryCore.parseAndValidateInput(input);
-            // console.log(chalk.green("Converted: "), chalk.yellow(result));
-            // stopLoadingSpinner();
-            // resolve(result);
-          } catch (error) {
-            console.error(chalk.red("Error processing file: "), error);
-            reject(error);
-          }
-        }
-      }
-    );
-  });
-}
-
-function runLexer() {
+export function runLexer() {
   console.time("Time");
 
   const fileContent = FileUtils.loadFile(
@@ -152,6 +42,4 @@ function runLexer() {
   console.timeEnd("Time");
 
   new Test().test();
-
-  // new Parser().produce(fileContent);
 }
--- packages/berrycore/src/index.ts
+++ packages/berrycore/src/index.ts
@@ -1,23 +1,7 @@
-#!/usr/bin/env node
-
-// initialize the application
-
-// import { Command } from "commander";
-// import { handleInput } from "./core/cli/cli";
-// import { handleInput } from "./core/cli/cli";
-
+import { runLexer } from "./core/cli/cli";
 import Parser from "./lang/ast/AstParser";
 import { Lexer } from "./lang/tokenizer/lexer";
 
-// console.log("FlexiBerry CLI v0.0.1");
-
-// const program = new Command();
-// program
-//   .version("0.0.1")
-//   .description("CLI to process JSON input")
-//   .option("-i, --input", "Input file string")
-//   .action(handleInput);
-
-// program.parse(process.argv);
+runLexer();
 
 export { Lexer, Parser };
